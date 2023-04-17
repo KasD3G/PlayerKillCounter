b1f78a703a348eb1d7f8257bd50da69799289206
KaszD3G <diego.ariza@colombiacompra.gov.co>
2023-04-17 02:38:17 -0500

    refactor: moving mod's data & methods to server side

--- media/lua/client/MainTest.lua
+++ media/lua/client/MainTest.lua
@@ -23,23 +23,27 @@ local adminTabId;
 
 local weekOffset = 15;
 
-local function updateWeekEvent()
-    local calendar = Calendar.getInstance();
-    local dayWeek = calendar:get(Calendar.DAY_OF_WEEK);
-    local weekYear = calendar:get(Calendar.WEEK_OF_YEAR);
-    local playerWeek =  tonumber(thisPlayer:getModData().PKCModData.week);
+-- local function updateWeekEvent()
+--     local calendar = Calendar.getInstance();
+--     local dayWeek = calendar:get(Calendar.DAY_OF_WEEK);
+--     local weekYear = calendar:get(Calendar.WEEK_OF_YEAR);
+--     local playerWeek = tonumber(thisPlayer:getModData().PKCModData.week);
 
-    if weekYear > playerWeek then
-        thisPlayer:getModData().PKCModData.week = weekYear;
-        print("LOG : PKC: week updated");
-    end
-end
+--     if weekYear > playerWeek then
+--         thisPlayer:getModData().PKCModData.week = weekYear;
+--         print("LOG : PKC: week updated");
+--     end
+-- end
 
 function PKCMain.updateUI()
-    print("LOG : PKC: updateUI");
-    local zText = "Kills: " .. tostring(thisPlayer:getModData().PKCModData.pvpKills) .. " <LINE> Points: " ..
-                      tostring(thisPlayer:getModData().PKCModData.pvpPoints) .. " <LINE> TK: " ..
-                      tostring(thisPlayer:getModData().PKCModData.pvpTK);
+    print("LOG : PKC: updating UI");
+    sendClientCommand(thisPlayer, "PKC", "updateUI", {});
+end
+
+local function drawUI(playerData)
+    print("LOG : PKC: drawUI");
+    local zText = "Kills: " .. tostring(playerData.pvpKills) .. " <LINE> Points: " .. tostring(playerData.pvpPoints) ..
+                      " <LINE> TK: " .. tostring(playerData.pvpTK);
     -- local zText = "TEST <LINE> TEST2"
     BoxTab.HomeWindow.text = zText;
     BoxTab.HomeWindow:paginate();
@@ -63,12 +67,12 @@ local function printMsg(msg, tabId)
     ChatSystem.addLineToChat(msg, msgColor, nil, nil, tabId);
 end
 
-local function chatPrintStats(tabId)
+local function chatPrintStats(dataPlayer)
     print("LOG : PKC: chatPrintStats");
-    local msg = "Kills: " .. tostring(thisPlayer:getModData().PKCModData.pvpKills) .. " | Points: " ..
-                    tostring(thisPlayer:getModData().PKCModData.pvpPoints) .. " | TK: " ..
-                    tostring(thisPlayer:getModData().PKCModData.pvpTK);
-    printMsg(msg, tabId);
+    local msg = "Kills: " .. tostring(dataPlayer.pvpKills) .. " | Points: " ..
+                    tostring(dataPlayer.pvpPoints) .. " | TK: " ..
+                    tostring(dataPlayer.pvpTK);
+    printMsg(msg, lastTabId);
 end
 
 function Commands.OnAddMessage(chatMessage, tabId)
@@ -85,15 +89,19 @@ function Commands.OnAddMessage(chatMessage, tabId)
 
         if commandArgs[2] == "evento" then
             print("LOG : PKC: evento");
-            chatPrintStats(tabId);
+            lastTabId = tabId;
+            sendClientCommand(thisPlayer, "PKC", "chatPrintStats", {});
+
         elseif thisPlayer:getAccessLevel() == "Admin" and commandArgs[2] == "admin" then
             print("LOG : PKC: admin command " .. tostring(thisPlayer:getAccessLevel()));
             adminTabId = tabId;
             sendClientCommand(thisPlayer, "PKC", "admin", {});
+
         else
             print("LOG : PKC: command not found");
             print("LOG : PKC: color=" .. tostring(chatMessage:getTextColor()));
             ISChat.addLineInChat(chatMessage, tabId);
+
         end
     else
         ISChat.addLineInChat(chatMessage, tabId);
@@ -101,134 +109,19 @@ function Commands.OnAddMessage(chatMessage, tabId)
 
 end
 
-local function tempSave()
-    print("-------------------------SAVING----------------------------------");
-    local writeData = getModFileWriter("PKC", "Data/PKCdata-" .. thisPlayer:getUsername() .. ".txt", true, false);
-    writeData:write("===DON'T EDIT THIS FILE===" .. "\n");
-    writeData:write(tonumber(thisPlayer:getModData().PKCModData.pvpKills) .. "\n");
-    writeData:write(tonumber(thisPlayer:getModData().PKCModData.pvpPoints) .. "\n");
-    writeData:write(tonumber(thisPlayer:getModData().PKCModData.pvpTK) .. "\n");
-    writeData:write(tonumber(thisPlayer:getModData().PKCModData.week) .. "\n");
-
-    local tbl = thisPlayer:getModData().PKCModData.weekDeaths;
-
-    if tbl ~= nil then
-        for k, v in pairs(tbl) do
-            writeData:write(k .. " %%% " .. tonumber(v) .. "\n");
-        end
-    end
-
-    writeData:close();
-end
-
 local function onPlayerDead(player)
     if thisPlayer:getUsername() == player:getUsername() then
         print("LOG : PKC: onPlayerDead=" .. tostring(player:getUsername()));
-        thisModData = thisPlayer:getModData().PKCModData;
-        tempSave();
+        -- thisModData = thisPlayer:getModData().PKCModData;
+        -- tempSave();
     end
-end
-
-local function deleteTempFile()
-    local writeData = getModFileWriter("PKC", "Data/PKCdata-" .. thisPlayer:getUsername() .. ".txt", true, false);
-    writeData:write("");
-    writeData:close();
-end
-
-local function readFile()
-    print("-------------------------READING FILE---------------------------------");
-    local readData = getModFileReader("PKC", "Data/PKCdata-" .. thisPlayer:getUsername() .. ".txt", false);
-    if readData ~= nil then
-        print("-------------------------1---------------------------------");
-        if readData:readLine() ~= "===DON'T EDIT THIS FILE===" then
-            print("LOG : PKC: ERROR: File is corrupted");
-            return;
-        end
-        print("-------------------------2---------------------------------");
-        thisPlayer:getModData().PKCModData = {};
-        print("-------------------------3---------------------------------");
-        thisPlayer:getModData().PKCModData.pvpKills = tonumber(readData:readLine());
-        print("-------------------------4---------------------------------");
-        thisPlayer:getModData().PKCModData.pvpPoints = tonumber(readData:readLine());
-        print("-------------------------5---------------------------------");
-        thisPlayer:getModData().PKCModData.pvpTK = tonumber(readData:readLine());
-        print("-------------------------6---------------------------------");
-        thisPlayer:getModData().PKCModData.week = tonumber(readData:readLine());
-
-        thisPlayer:getModData().PKCModData.weekDeaths = {};
-        local line = readData:readLine();
-        print("-------------------------7---------------------------------");
-        while line ~= nil do
-            print("-------------------------8---------------------------------");
-            local tbl = line:split(" %%% ");
-            print("-------------------------9---------------------------------");
-            if tbl[1] ~= nil then
-                print("-------------------------10---------------------------------");
-                thisPlayer:getModData().PKCModData.weekDeaths[tbl[1]] = tonumber(tbl[2]);
-                print("-------------------------11---------------------------------");
-                print("LOG : PKC: tblTest[" .. tostring(tbl[1]) .. "] = " .. tostring(tbl[2]));
-            end
-            print("-------------------------12---------------------------------");
-            line = readData:readLine();
-        end
-        readData:close();
-        print("-------------------------READING FILE ENDDDD---------------------------------");
-        deleteTempFile();
-    else
-        print("-------------------------FILE EMPTY---------------------------------");
-        thisPlayer:getModData().PKCModData = {};
-    end
-    print("-------------------------13---------------------------------");
-end
-
-local function playerCreateModData(player)
-    if not player then
-        return
-    end
-    print("-------------------------CREATE MOD DATA----------------------------------");
-    if thisModData ~= nil then
-        print("-----------thisModData------------------")
-        player:getModData().PKCModData = thisModData;
-    end
-    if player:getModData().PKCModData == nil then
-
-        readFile();
-
-        if player:getModData().PKCModData == nil then
-            print("-----------PKCModData------------------")
-            player:getModData().PKCModData = {};
-        else
-            print("-----------SCRIPT DATA------------------")
-        end
-    end
-    if player:getModData().PKCModData.pvpKills == nil then
-        print("-----------pvpKills------------------")
-        player:getModData().PKCModData.pvpKills = 0;
-    end
-    if player:getModData().PKCModData.pvpPoints == nil then
-        print("-----------pvpPoints------------------")
-        player:getModData().PKCModData.pvpPoints = 0;
-    end
-    if player:getModData().PKCModData.pvpTK == nil then
-        print("-----------pvpTK------------------")
-        player:getModData().PKCModData.pvpTK = 0;
-    end
-    if player:getModData().PKCModData.weekDeaths == nil then
-        print("-----------weekDeaths------------------")
-        player:getModData().PKCModData.weekDeaths = {};
-    end
-    if player:getModData().PKCModData.week == nil then
-        print("-----------week------------------")
-        player:getModData().PKCModData.week = 1;
-    end
-    updateWeekEvent();
 end
 
 local function OnCreatePlayer(playerIndex, player)
     print("-------------------------CREATE PLAYER----------------------------------");
     thisPlayer = player
     print("LOG : thisPlayer=" .. tostring(thisPlayer:getUsername()));
-    playerCreateModData(player);
+    sendClientCommand(thisPlayer, "PKC", "createPlayer", {});
 end
 
 local function findKey(tbl, key)
@@ -254,25 +147,9 @@ local function OnWeaponHitCharacter(wielder, character, handWeapon, damage)
         else
             -- on hit zombie test here
             print("-------------------------TEST----------------------------------");
-            local dateFormat = SimpleDateFormat.new("H:mm");
-            print("LOG : PKC: isnewgame= " .. tostring(getGameTime()));
 
         end
     end
-end
-
-local function sumEventPoints(args)
-    thisPlayer:getModData().PKCModData.pvpKills = tonumber(thisPlayer:getModData().PKCModData.pvpKills) + 1;
-    thisPlayer:getModData().PKCModData.pvpPoints = tonumber(thisPlayer:getModData().PKCModData.pvpPoints) + 1;
-    thisPlayer:getModData().PKCModData.weekDeaths[args.victim] = tonumber(thisPlayer:getModData().PKCModData.week);
-    ChatSystem.addLineToChat("Punto por matar a " .. args.victim);
-end
-
-local function getStats()
-    local msg = "Kills: " .. tostring(thisPlayer:getModData().PKCModData.pvpKills) .. " | Points: " ..
-                    tostring(thisPlayer:getModData().PKCModData.pvpPoints) .. " | TK: " ..
-                    tostring(thisPlayer:getModData().PKCModData.pvpTK);
-    return msg;
 end
 
 local function OnServerCommand(module, command, args)
@@ -285,40 +162,50 @@ local function OnServerCommand(module, command, args)
             print("-------------------------ITS ME!!!----------------------------------");
             print("LOG : PKC: attacek= " .. args.attacker);
             print("LOG : PKC: victim= " .. args.victim);
-            local tbl = thisPlayer:getModData().PKCModData
+            print("LOG : PKC: msg= " .. args.msg);
 
-            if tbl.weekDeaths[args.victim] == nil then
-                print("-------------------------KILL COUNT FIRST TIME----------------------------------");
-                sumEventPoints(args);
-            elseif tonumber(tbl.weekDeaths[args.victim]) < tonumber(tbl.week) then
-                print("-------------------------KILL COUNT----------------------------------");
-                sumEventPoints(args);
+            if args.tempWeek ~= nil then
+                ChatSystem.addLineToChat("Punto por matar a " .. args.victim);
             else
-                print("--------------------sss-----KILL COUNT NOT ADDED----------------------------------");
-                local tempWeek = tonumber(thisPlayer:getModData().PKCModData.week) - weekOffset;
-                ChatSystem.addLineToChat(args.victim .. " NO da mas puntos para la semana " .. tempWeek, "<RGB:1,1,1>");
+                ChatSystem.addLineToChat(args.victim .. " NO da mas puntos para la semana " .. args.tempWeek);
             end
-            thisPlayer:getModData().PKCModData.pvpTK = tonumber(thisPlayer:getModData().PKCModData.pvpTK) + 1;
+
             PKCMain.updateUI();
         end
     elseif command == "getStats" then
         print("-------------------------GET STATS----------------------------------");
-        args.stats = getStats();
-        args.player = thisPlayer:getUsername();
-        sendClientCommand(thisPlayer, "PKC", "broadcast", args);
-    elseif command == "broadcast" then
-        print("-------------------------BROADCAST----------------------------------");
         if args.admin == thisPlayer:getUsername() then
-            ChatSystem.addLineToChat(args.player .. " = " .. args.stats, "<RGB:1,1,1>", nil, nil, adminTabId);
+            ChatSystem.addLineToChat("------ Server Event Stats ------", "<RGB:1,1,1>", nil, nil, adminTabId);
+            ChatSystem.addLineToChat(args.msg, "<RGB:1,1,1>", nil, nil, adminTabId);
+        end
+    elseif command == "createPlayer" then
+        if args.player == thisPlayer:getUsername() then
+            print("-------------------------CREATE PLAYER----------------------------------");
+            print(args.msg);
+        end
+    elseif command == "broadcast" then
+        -- print("-------------------------BROADCAST----------------------------------");
+        -- if args.admin == thisPlayer:getUsername() then
+        --     ChatSystem.addLineToChat(args.player .. " = " .. args.stats, "<RGB:1,1,1>", nil, nil, adminTabId);
+        -- end
+    elseif command == "updateUI" then
+        if args.player == thisPlayer:getUsername() then
+            if args.playerData ~= nil then
+                drawUI(args.playerData);
+            else
+                print("LOG : PKC: ERROR: updateUI args.playerData is nil");
+            end
+        end
+    elseif command == "chatPrintStats" then
+        if args.player == thisPlayer:getUsername() then
+            if args.playerData ~= nil then
+                chatPrintStats(args.playerData);
+            else
+                print("LOG : PKC: ERROR: chatPrintStats args.playerData is nil");
+            end
         end
     end
 end
-
-local function EveryOneMinute()
-	-- Your code here
-end
-
-
 
 new_function = function()
     if not isClient() then
@@ -355,4 +242,4 @@ Events.OnCreatePlayer.Add(OnCreatePlayer);
 Events.OnWeaponHitCharacter.Add(OnWeaponHitCharacter);
 -- Events.OnCharacterDeath.Add(OnCharacterDeath)
 Events.OnPlayerDeath.Add(onPlayerDead);
-Events.EveryTenMinutes.Add(updateWeekEvent);
+-- Events.EveryTenMinutes.Add(updateWeekEvent);
--- media/lua/server/MainServer.lua
+++ media/lua/server/MainServer.lua
@@ -2,6 +2,9 @@ if isClient() then
     return;
 end
 
+local weekOffset = 15;
+local week = 1;
+
 local function PkcModData()
     print("-----OnInitGlobalModData------")
     if not ModData.exists("PKCData") then
@@ -11,19 +14,157 @@ local function PkcModData()
         local dateStamp = Calendar.getInstance():getTime();
         local dateFormat = SimpleDateFormat.new("dd/MM/yyyy HH:mm:ss");
         t.creationDate = tostring(dateFormat:format(dateStamp) or "N/A");
+        t.data = {};
     end
 end
 
+local function WeekEvent()
+    local calendar = Calendar.getInstance();
+    local weekYear = calendar:get(Calendar.WEEK_OF_YEAR);
+
+    return weekYear
+end
+
+local function sumEventPoints(playerData, victim)
+    playerData.weekDeaths[victim] = WeekEvent();
+    playerData.pvpPoints = playerData.pvpPoints + 1;
+    playerData.pvpKills = playerData.pvpKills + 1;
+
+    return playerData;
+end
+
+local function setModDataPlayer(steamID, player, victim)
+    local tbl = ModData.get("PKCData");
+    local msg = "";
+    local tempWeek = nil
+
+    if tbl["data"] == nil then
+        tbl.data = {};
+        msg = msg .. " Creating data table";
+    end
+
+    if tbl.data[steamID] == nil then
+        tbl.data[steamID] = {};
+        msg = msg .. " Creating steamID: " .. steamID;
+    end
+
+    local steamData = tbl.data[steamID];
+    if steamData[player] == nil then
+        tbl.data[steamID][player] = {};
+        msg = msg .. " Creating player: " .. player;
+    end
+
+    local playerData = steamData[player];
+
+    if playerData["pvpKills"] == nil then
+        tbl.data[steamID][player]["pvpKills"] = 0;
+        msg = msg .. " Creating pvpKills";
+    end
+
+    if playerData["pvpPoints"] == nil then
+        tbl.data[steamID][player]["pvpPoints"] = 0;
+        msg = msg .. " Creating pvpPoints";
+    end
+
+    if playerData["pvpTK"] == nil then
+        tbl.data[steamID][player]["pvpTK"] = 0;
+        msg = msg .. " Creating pvpTK";
+    end
+
+    if playerData["weekDeaths"] == nil then
+        tbl.data[steamID][player]["weekDeaths"] = {};
+        msg = msg .. " Creating weekDeaths";
+    end
+
+    local tblWeekDeaths = playerData["weekDeaths"];
+
+    if victim ~= nil then
+
+        if tblWeekDeaths[victim] == nil then
+            tbl.data[steamID][player] = sumEventPoints(tbl.data[steamID][player], victim);
+            msg = " KILL COUNT FIRST TIME"
+        elseif tblWeekDeaths[victim] < WeekEvent() then
+            tbl.data[steamID][player] = sumEventPoints(tbl.data[steamID][player], victim);
+            msg = " KILL COUNT"
+        else
+            tempWeek = WeekEvent() - weekOffset;
+            msg = " KILL COUNT NOD ADDED"
+        end
+        tbl.data[steamID][player].pvpTK = tbl.data[steamID][player].pvpTK + 1;
+    end
+
+    return {
+        msg = msg,
+        tempWeek = tempWeek
+    };
+end
+
+local function getPlayerData(steamID, player)
+    local tbl = ModData.get("PKCData");
+
+    if tbl.data[steamID] == nil then
+        return nil;
+    end
+
+    if tbl.data[steamID][player] == nil then
+        return nil;
+    end
+
+    return tbl.data[steamID][player];
+end
+
+local function getAllPlayersData()
+    local tbl = ModData.get("PKCData");
+    local msgData = nil;
+
+    for steamID, steamData in ipairs(tbl.data) do
+        for player, dataPlayer in ipairs(steamData) do
+            local msg = tostring(player) .. " : Kills: " .. tostring(dataPlayer.pvpKills) .. " | Points: " ..
+                            tostring(dataPlayer.pvpPoints) .. " | TK: " .. tostring(dataPlayer.pvpTK);
+            if msgData == nil then
+                msgData = msg;
+            else
+                msgData = msgData .. "<LINE>" .. msg;
+            end
+        end
+    end
+
+    return msgData;
+end
+
 local function OnClientCommand(module, command, player, args)
-    
+
     if module ~= "PKC" then
         return;
     end
 
     if command == "admin" then
-        sendServerCommand("PKC", "getStats", {admin=player:getUsername()});
+        sendServerCommand("PKC", "getStats", {
+            admin = player:getUsername(),
+            msg = getAllPlayersData()
+        });
+    elseif command == "createPlayer" then
+        local steamID = player:getSteamID();
+        local playerName = player:getUsername();
+        local data = setModDataPlayer(steamID, playerName, nil);
+
+        sendServerCommand("PKC", "createPlayer", {
+            player = playerName,
+            msg = data.msg,
+            tempWeek = data.tempWeek
+        });
     elseif command == "broadcast" then
-        sendServerCommand("PKC", "broadcast", args);
+        -- sendServerCommand("PKC", "broadcast", args);
+    elseif command == "updateUI" then
+        sendServerCommand("PKC", "updateUI", {
+            playerData = getPlayerData(player:getSteamID(), player:getUsername()),
+            player = player:getUsername()
+        });
+    elseif command == "chatPrintStats" then
+        sendServerCommand("PKC", "chatPrintStats", {
+            playerData = getPlayerData(player:getSteamID(), player:getUsername()),
+            player = player:getUsername()
+        });
     end
 
 end
@@ -35,7 +176,7 @@ local function OnCharacterDeath(player)
         print("LOG_SERVER : PKC : attacker " .. tostring(player:getAttackedBy()));
         print("LOG_SERVER : PKC : isOnFire " .. tostring(player:isOnFire()));
         local victim = player:getUsername();
-        
+
         if player:getAttackedBy() == nil then
             print("LOG : PKC: " .. victim .. " es un tonto y se ha suicidado");
         elseif player:getAttackedBy():isZombie() then
@@ -43,7 +184,14 @@ local function OnCharacterDeath(player)
         else
             local attacker = player:getAttackedBy():getUsername();
             print("LOG : PKC: " .. victim .. " was killed by " .. attacker);
-            sendServerCommand("PKC", "PlayerDeath", {victim=victim, attacker=attacker});
+
+            local data = setModDataPlayer(player:getSteamID(), attacker, victim);
+            sendServerCommand("PKC", "PlayerDeath", {
+                msg = data.msg,
+                tempWeek = data.tempWeek,
+                victim = victim,
+                attacker = attacker
+            });
         end
 
     end
